d25dd44b5095c191fc8b6565870175642931f1f8
darshanvirani007 <darshanvirani2468@gmail.com>
2026-09-18 00:48:55 +0100

    leetcode 0167 sliding window pattern

---
+++ array_hashing/0128_longest_consecutive_sequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        numbers = set(nums)
+
+        longest = 0
+
+        for num in numbers:
+            if num - 1 not in numbers:
+                length = 1
+
+                while num + length in numbers:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
